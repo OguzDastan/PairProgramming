139c68bef9c05fbb68293420e88b0bf2d6b5088e
Oguz Dastan <oguzx@live.dk>
2019-10-22 09:37:21 +0200

    asd
    asd

---
+++ MusicLib/MusicRecords.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MusicLib
+{
+    public class MusicRecords
+    {
+        private string _title;
+        private string _artist;
+        private double _duration;
+        private DateTime _publishDate;
+        private string _genre;
+
+        public MusicRecords(string title, string artist, double duration, DateTime publishdate, string genre)
+        {
+            title = Title;
+            artist = Artist;
+            duration = Duration;
+            publishdate = PublishDate;
+            genre = Genre;
+        }
+
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set => _artist = value;
+        }
+
+        public double Duration
+        {
+            get => _duration;
+            set => _duration = value;
+        }
+
+        public DateTime PublishDate
+        {
+            get => _publishDate;
+            set => _publishDate = value;
+        }
+
+        public string Genre
+        {
+            get => _genre;
+            set => _genre = value;
+        }
+    }
+}
---
+++ PairProgrammingRest/Controllers/RecordController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicLib;
+
+namespace PairProgrammingRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordController : ControllerBase
+    {
+        private static List<MusicRecords> mList = new List<MusicRecords>()
+        {
+            new MusicRecords("","",0,DateTime.Now, "")
+        };
+        // GET: api/Record
+        [HttpGet]
+        public IEnumerable<MusicRecords> Get()
+        {
+            return mList;
+        }
+
+        // GET: api/Record/5
+        [HttpGet("{title}", Name = "Get")]
+        public MusicRecords Get(string title)
+        {
+            return mList.Find(c => c.Title == title);
+        }
+
+        // POST: api/Record
+        [HttpPost]
+        public void Post([FromBody] MusicRecords value)
+        {
+            mList.Add(value);
+        }
+
+        // PUT: api/Record/5
+        [HttpPut("{title}")]
+        public void Put(string title, [FromBody] MusicRecords value)
+        {
+            MusicRecords mr = Get(title);
+            if (mr != null)
+            {
+                mr.Title = value.Title;
+                mr.Artist = value.Artist;
+                mr.Duration = value.Duration;
+                mr.PublishDate = value.PublishDate;
+                mr.Genre = value.Genre;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{title}")]
+        public void Delete(string title)
+        {
+            MusicRecords mr = Get(title);
+            if (mr != null)
+            {
+                mList.Remove(mr);
+            }
+        }
+    }
+}
